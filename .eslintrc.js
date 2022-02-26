0aeac4c7fd0c6bbb86c68c2bb35577071fc87907
huitoutunao <lh_huitoutunao@163.com>
2022-02-26 15:47:02 +0800

    '更新eslint配置'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,40 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/airbnb',
+    '@vue/prettier',
+    '@vue/eslint-config-prettier',
   ],
   parserOptions: {
+    ecmaVersion: 2020,
     parser: '@babel/eslint-parser',
+    sourceType: 'module',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
+    semi: ['error', 'never'],
   },
+  settings: {},
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true,
       },
     },
   ],
-};
+}
